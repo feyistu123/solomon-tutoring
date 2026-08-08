dfc858ae5f805b483358cbd3f0be0d667ffe983b
Feyistu <fayistuendale@gmail.com>
2026-08-08 14:09:35 +0300

    fix connection timeout error with email reply

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -8,8 +8,8 @@ const authenticateToken = require('../middleware/authMiddleware');
 function createTransporter() {
   return nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
     tls: { rejectUnauthorized: false }
   });
